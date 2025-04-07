c36c2763538f4d66d72fb2fd501cd8d6fe6b9440
andooo26 <yota426513@gmail.com>
2025-04-07 15:08:44 +0900

    nextauthって分けて書くのがAppRouterのお作法らしい

---
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -0,0 +1,17 @@
+import GoogleProvider from 'next-auth/providers/google';
+
+export const authOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async signIn({ user }) {
+      const domain = user.email?.split('@')[1];
+      return domain === process.env.AUTHORIZED_DOMAIN;
+    },
+  },
+};
